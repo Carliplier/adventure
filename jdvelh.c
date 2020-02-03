a477b46cb8243d1d21e906e2e64bfe37dfa95aa3
Carliplier <charles.remande@gmail.com>
2020-02-03 15:05:15 +0100

    adventure

---
+++ jdvelh.c
@@ -0,0 +1,26 @@
+
+
+
+#include <stdio.h>
+
+int main(){
+    
+int choix=50;
+
+
+printf("Bonjour voyageur, où désirez vous aller ? \n Aller dans la vallée Duhommet (1) \n Aller dans la caverne Gothard (2)");
+    
+scanf("%d", &choix);
+
+
+
+
+if (choix==1){
+    printf("Vous entrez dans la vallée Duhommet\n");
+    }
+
+else if(choix==2){
+    printf("Vous entrez dans la caverne Gothard \n");
+    }
+
+}
